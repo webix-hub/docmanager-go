63708028b39862825a636131a9997341b25a868a
Nastya <akovalenko@xbsoftware.com>
2020-07-06 20:58:09 +0300

    [dev] getting folders for branches; replacing newlines only in unequal diffs

--- diff.go
+++ diff.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bytes"
-	diffLib "github.com/sergi/go-diff/diffmatchpatch"
 	"html"
 	"strings"
+
+	diffLib "github.com/sergi/go-diff/diffmatchpatch"
 )
 
 var replacer *strings.Replacer
@@ -19,15 +20,15 @@ func diffHTML(text1, text2 string) string {
 
 	var buff bytes.Buffer
 	for _, diff := range diffs {
-		text := replacer.Replace(html.EscapeString(diff.Text))
+		text := html.EscapeString(diff.Text)
 		switch diff.Type {
 		case diffLib.DiffInsert:
 			_, _ = buff.WriteString("<span class='webix_docmanager_diff_insert'>")
-			_, _ = buff.WriteString(text)
+			_, _ = buff.WriteString(replacer.Replace(text))
 			_, _ = buff.WriteString("</span>")
 		case diffLib.DiffDelete:
 			_, _ = buff.WriteString("<span class='webix_docmanager_diff_remove'>")
-			_, _ = buff.WriteString(text)
+			_, _ = buff.WriteString(replacer.Replace(text))
 			_, _ = buff.WriteString("</span>")
 		case diffLib.DiffEqual:
 			_, _ = buff.WriteString("<span>")
