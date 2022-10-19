da6bacff592b958c387f69cb44fee6424f6bed02
vkovalev <vkovalev@xbsoftware.by>
2022-10-19 14:43:29 +0300

    [dev] support old routes for backwards compatibility

--- server.go
+++ server.go
@@ -133,6 +133,14 @@ func main() {
 	addFilesRoutes(r)
 	addTrashRoutes(r)
 
+	r.Get("/icons/{size}/{type}/{name}", func(w http.ResponseWriter, r *http.Request) {
+		size := chi.URLParam(r, "size")
+		name := chi.URLParam(r, "name")
+		ftype := chi.URLParam(r, "type")
+
+		http.ServeFile(w, r, getIconURL(size, ftype, name, "none"))
+	})
+
 	r.Get("/icons/{skin}/{size}/{type}/{name}", func(w http.ResponseWriter, r *http.Request) {
 		skin := chi.URLParam(r, "skin")
 		size := chi.URLParam(r, "size")
